c72f98a833a36856ee44a7516379382349ef6f16
yeargor <egor.senkevichh@gmail.com>
2025-06-24 01:19:37 +0300

    exit from development mode

--- WebRoutes/Program.cs
+++ WebRoutes/Program.cs
@@ -151,15 +151,12 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ByWays API v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "ByWays API v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseAuthentication();
 
